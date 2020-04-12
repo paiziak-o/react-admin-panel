5e190496f3ef527f841877d045596e24dd0b7c70
paiziak-o <paiziak.o@gmail.com>
2020-04-12 22:31:06 +0300

    Add icons to header

--- src/components/elements/page/Header.tsx
+++ src/components/elements/page/Header.tsx
@@ -21,11 +21,35 @@ const Header: React.FunctionComponent = () => {
           <Button className="header-search-btn">Search</Button>
         </Form>
         <Nav>
-          <Nav.Link href={url.dashboard}><i className="fas fa-bell"></i></Nav.Link>
-          <NavDropdown alignRight title="Account" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <Nav.Link href={url.dashboard}>
+            <i className="fas fa-bell"></i>
+            <span className="badge badge-danger badge-counter">3+</span>
+          </Nav.Link>
+          <Nav.Link href={url.dashboard}>
+            <i className="fas fa-envelope fa-fw"></i>
+            <span className="badge badge-danger badge-counter">3+</span>
+          </Nav.Link>
+          <NavDropdown
+            alignRight
+            title={<>
+              <span className="mr-2 d-none d-lg-inline text-gray-600 small" />
+              <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/30x30" />
+            </>}
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item href="#action/3.1">
+              <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+              Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">
+              <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> 
+              Settings
+            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item onClick={() => dispatch(logout())}>Logout</NavDropdown.Item>
+            <NavDropdown.Item onClick={() => dispatch(logout())}>
+              <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
