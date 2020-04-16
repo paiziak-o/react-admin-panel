387510a9a6601bb9d125a50a1ba70b8906c658ee
paiziak-o <paiziak.o@gmail.com>
2020-04-16 11:36:13 +0300

    Process on sidebar

--- src/components/elements/page/Sidebar.tsx
+++ src/components/elements/page/Sidebar.tsx
@@ -5,10 +5,34 @@ import {
 
 const Sidebar: React.FunctionComponent = () => (
   <Navbar expand="lg" className="admin-sidebar bg-gradient-primary">
-    <Nav.Item><Nav.Link>
-      <i className="fas fa-fw fa-table" />
-      <span>Products</span></Nav.Link></Nav.Item>
+    <Nav.Item>
+      <Nav.Link>
+        <i className="fas fa-fw fa-table" />
+        <span>Products</span>
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link>
+        <i className="fas fa-fw fa-table" />
+        <span>Categories</span>
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link>
+        <i className="fas fa-fw fa-table" />
+        <span>Orders</span>
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link>
+        <i className="fas fa-fw fa-table" />
+        <span>Users</span>
+      </Nav.Link>
+    </Nav.Item>
     <hr className="sidebar-divider d-none d-md-block" />
+    <div className="text-center d-none d-md-inline rounded-circle border-0">
+      <i className="fas fa-angle-right"></i>
+    </div>
   </Navbar>
 );
 
