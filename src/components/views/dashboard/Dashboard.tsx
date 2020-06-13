a6c293ba9b40904125deeeca9bb0a72335621579
paiziak-o <paiziak.o@gmail.com>
2020-06-13 15:59:28 +0300

    Add langs

--- src/components/views/dashboard/Dashboard.tsx
+++ src/components/views/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React, { ReactNode } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer, PieChart, Pie } from 'recharts';
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
 
 import AdminWrapper from '../AdminWrapper';
-import RequiresAuth from '../../hoc/RequiresAuth';
 import Box from './Box';
 
 interface Props {
@@ -28,57 +29,61 @@ const data2 = [
 ];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const Dashboard: React.FC = () => (
-  <AdminWrapper wrapperClass="dashbord">
-    
-      <Row>
-        <Col className="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-          <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-            <i className="fas fa-download fa-sm text-white-50"></i> Generate Report
-          </a>
-        </Col>
-      </Row>
-      <Row>
-        <Box icon="fas fa-calendar" title="EARNINGS (MONTHLY)" price="$40,000" />
-        <Box icon="fas fa-dollar-sign" title="EARNINGS (ANNUAL)" price="$215,000" />
-        <Box icon="fas fa-clipboard-list" title="TASKS" price="50" />
-        <Box icon="fas fa-comments" title="PENDING REQUESTS" price="18" />
-      </Row>
-      <Row className="row-eq-height">
-        <Col xl={8} lg={7}>
-          <div className="card shadow mb-4">
-            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-            </div>
-            <div className="card-body">
-              <div className="chart-area">
-                <ResponsiveContainer width='100%' height={300}>
-                  <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                  </LineChart>
-                </ResponsiveContainer>
+const Dashboard: React.FC = () => {
+  const { t } = useTranslation();
+
+  return(
+    <AdminWrapper wrapperClass="dashbord">
+      
+        <Row>
+          <Col className="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 className="h3 mb-0 text-gray-800">{t('dashboard.title')}</h1>
+            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+              <i className="fas fa-download fa-sm text-white-50"></i> Generate Report
+            </a>
+          </Col>
+        </Row>
+        <Row>
+          <Box icon="fas fa-calendar" title="EARNINGS (MONTHLY)" price="$40,000" />
+          <Box icon="fas fa-dollar-sign" title="EARNINGS (ANNUAL)" price="$215,000" />
+          <Box icon="fas fa-clipboard-list" title="TASKS" price="50" />
+          <Box icon="fas fa-comments" title="PENDING REQUESTS" price="18" />
+        </Row>
+        <Row className="row-eq-height">
+          <Col xl={8} lg={7}>
+            <div className="card shadow mb-4">
+              <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+              </div>
+              <div className="card-body">
+                <div className="chart-area">
+                  <ResponsiveContainer width='100%' height={300}>
+                    <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                    </LineChart>
+                  </ResponsiveContainer>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col xl={4} lg={5}>
-          <div className="card shadow mb-4">
-            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 className="m-0 font-weight-bold text-primary">Some Note</h6>
+          </Col>
+          <Col xl={4} lg={5}>
+            <div className="card shadow mb-4">
+              <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 className="m-0 font-weight-bold text-primary">Some Note</h6>
+              </div>
+              <div className="card-body">
+                <p>
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.."
+                </p>
+              </div>
             </div>
-            <div className="card-body">
-              <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.."
-              </p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-  </AdminWrapper>
-);
+          </Col>
+        </Row>
+    </AdminWrapper>
+  );
+}
 
 export default Dashboard;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import i18n from "i18next";
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from "react-i18next";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .init({
+    backend: {
+      loadPath: '../lang/{{lng}}.json',
+    },
+    lng: "en",
+    fallbackLng: "en",
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 ReactDOM.render(
   <Provider store={store}>
